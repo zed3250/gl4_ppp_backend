18cddbcfa230f5aeef0e6431f104818593d476a9
mmmarzouki <mmmarzouki@gmail.com>
2019-05-03 13:20:21 +0200

    added method to create folder

--- app/Controllers/Http/DocumentController.js
+++ app/Controllers/Http/DocumentController.js
@@ -1,10 +1,27 @@
 'use strict';
-
+var Minio = require('minio');
+const Env = use('Env');
+/**
+ * Minio Client
+ */
+const minioClient = new Minio.Client({
+    endPoint: 'localhost',
+    port: 9000,
+    useSSL: false,
+    accessKey: Env.get('MINIO_ACCESS_KEY'),
+    secretKey: Env.get('MINIO_SECRET_KEY'),
+});
 /**
  * Controller to create and get files from the minio server
  */
 class DocumentController {
 
-    
-} 
+    createFolder() {
+
+        minioClient.makeBucket('europetrip', 'us-east-1', function (err) {
+            if (err) return console.log(err)
+            console.log('Bucket created successfully in "us-east-1".')
+        });
+    }
+}
 module.exports = DocumentController;
