db54a8cf8fe0aa840422468469fe48ca4194fc6a
mmmarzouki <mmmarzouki@gmail.com>
2019-05-04 18:42:52 +0200

    added method to create document

--- app/Controllers/Http/DocumentController.js
+++ app/Controllers/Http/DocumentController.js
@@ -1,26 +1,46 @@
-'use strict';
-var Minio = require('minio');
-const Env = use('Env');
+"use strict";
+const Minio = require("minio");
+const Env = use("Env");
+const Fs = require("fs");
+const Helpers = use("Helpers");
 /**
  * Minio Client
  */
 const minioClient = new Minio.Client({
-    endPoint: 'localhost',
-    port: 9000,
-    useSSL: false,
-    accessKey: Env.get('MINIO_ACCESS_KEY'),
-    secretKey: Env.get('MINIO_SECRET_KEY'),
+  endPoint: "localhost",
+  port: 9000,
+  useSSL: false,
+  accessKey: Env.get("MINIO_ACCESS_KEY"),
+  secretKey: Env.get("MINIO_SECRET_KEY")
 });
 /**
  * Controller to create and get files from the minio server
  */
 class DocumentController {
-
-    createDocument({ request, response }) {
-
-        const body = request.all();
-
-        
+  async createDocument({ request, response }) {
+    const file = request.file("file");
+    const bucket = request.input("project") + "-" + request.input("process");
+    await file.move(Helpers.tmpPath(), {
+      name: "temp.pdf",
+      overwrite: true
+    });
+    if (!file.moved()) {
+      return file.error();
     }
+    const path = Helpers.tmpPath('temp.pdf');
+    var fileStream = Fs.createReadStream(path);
+
+    minioClient.putObject(
+      bucket,
+      file.fileName,
+      fileStream,
+      function(err, etag) {
+        return console.log(err, etag); // err should be null
+      }
+    );
+
+    Fs.unlinkSync(path);
+    return "File uploaded";
+  }
 }
 module.exports = DocumentController;
