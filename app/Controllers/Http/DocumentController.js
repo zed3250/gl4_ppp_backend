70c7314534d962ce7a28b7e716384336edff8115
mmmarzouki <mmmarzouki@gmail.com>
2019-05-06 16:02:45 +0200

    updating document creation & getting not using minio anymore

--- app/Controllers/Http/DocumentController.js
+++ app/Controllers/Http/DocumentController.js
@@ -1,83 +1,51 @@
 "use strict";
-const Minio = require("minio");
 const Env = use("Env");
 const fs = require("fs");
 const Helpers = use("Helpers");
-/**
- * Minio Client
- */
-const minioClient = new Minio.Client({
-  endPoint: "localhost",
-  port: 9000,
-  useSSL: false,
-  accessKey: Env.get("MINIO_ACCESS_KEY"),
-  secretKey: Env.get("MINIO_SECRET_KEY")
-});
+
 /**
  * Controller to create and get files from the minio server
  */
 
-function randomString(length) {
-  var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
-
-
 class DocumentController {
 
    async createDocument({ request }) {
-    var document = new  Document();
+    var document ;
+    var project = Project.query().where({_id: input("project")}).first();
+    var process = input('process');
+
+
     document.docName = request.input("docName");
     document.docType = request.input('docType');
-    document.docPath = request.input("project") + "-" + request.input("process");
-    await document.save();
+    const path = Helpers.publicPath(params.project + "/" + params.process + "/" + params.document);
+    document.docPath = path;
+    document.permittedRoles = request.input('roles');
+    
+    project.processes.forEach(element => {
+      if (element._id == process) {
+        element.docs.push(document);
+      }
+    });
+
+    await project.save();
+
     const file = request.file("file");
-    const bucket = request.input("project") + "-" + request.input("process");
     const name = request.input("docName");
-    var random = randomString(6);
-    random += ".pdf";
-    await file.move(Helpers.tmpPath(), {
-      name: random,
+
+    await file.move(path, {
+      name: name,
       overwrite: true
     });
     if (!file.moved()) {
       return file.error();
     }
-    const path = Helpers.tmpPath(random);
-    var fileStream = fs.createReadStream(path);
-
-    minioClient.putObject(bucket, name, fileStream, function(err, etag) {
-      if (err) return console.log(err, etag); // err should be null
-    });
-
-    fs.unlinkSync(path);
-    return "File uploaded";
+    return "document created";
   }
 
-  async getDocument({ params , response }) {
-    const path = params.project + "-" + params.process;
-    const name = params.document;
-    var random = randomString(6);
-    random += ".pdf";
-    const tempPath = Helpers.tmpPath(random);
-
-    minioClient.fGetObject(path, name, tempPath, (err) => {
-      if (err) {
-        return console.log(err);
-      }
-      console.log('recieved');
-      console.log('sending');
-    });
-    setTimeout(() => {
-      response.header('Content-Type', 'application/pdf');
-      console.log('sent');
-      response.download(tempPath, name);
-    }, 1500);
+  getDocument({ params , response }) {
+    const fullPath = params.input('path');
+    response.header('Content-Type', 'application/pdf');
+    response.download(fullPath, name);
   }
   
 
